0847c9576357b8f2585d901a76f6b7a0c97fda67
Serge <lvsbud@ukr.net>
2020-08-17 13:50:54 +0300

    1-st commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// "use strict";
+
+// alert('bvdfsj');
+// const result = confirm('we use coockies at site');
+
+// console.log(result);
+
+// const answers = [];
+// answers[0] = prompt('Имя', ''); 
+// answers[1] = prompt('Фамилия', '');
+// answers[2] = +prompt('Возраст', '');
+
+// document.write(answers);
+
+// let mcategory = prompt('category_name', '');
+// let prodduct = prompt('product_name', '');
+
+// alert(`${mcategory }/${prodduct}`);
+
+// document.write(`https://myshop/${mcategory }/${prodduct}`);
+
+// ----------------------------------------
+
+
+// let incr = 10,
+//     decr = 6;
+
+//     console.log(++incr , --decr);
